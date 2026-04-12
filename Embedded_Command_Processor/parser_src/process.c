634965817e152567ccf84594e0a6c0f890825994
Likith <likithrgowda8@gmail.com>
2026-04-12 20:05:13 +0530

    stopped \0 from transmitting via usart

--- Embedded_Command_Processor/parser_src/process.c
+++ Embedded_Command_Processor/parser_src/process.c
@@ -27,29 +27,29 @@ void extract_cmd(r_buffer* rb){
 }
 
 void process_cmd(char* cmd){
-    char* cmd_list[3]={"led on","led off","status"};
+    const char* cmd_list[3]={"led on","led off","status"};
     static uint8_t led_status=0;
 if(strcmp(cmd_list[0],cmd)==0){
     //printf("led turned on\n");
-	USART_SendData_IT(&g_usart3, (uint8_t*)"led on\n\r", sizeof("led on\n\r"));
+	USART_SendData_IT(&g_usart3, (uint8_t*)"led on\n\r", sizeof("led on\n\r")-1);
 	GPIO_WritePin(g_led3.pGPIOx, 14, ENABLE);
 	led_status=ENABLE;
 }
 else if(strcmp(cmd_list[1],cmd)==0){
-	USART_SendData_IT(&g_usart3, (uint8_t*)"led off\n\r", sizeof("led off\n\r"));
+	USART_SendData_IT(&g_usart3, (uint8_t*)"led off\n\r", sizeof("led off\n\r")-1);
 	GPIO_WritePin(g_led3.pGPIOx, 14, DISABLE);
 	led_status=DISABLE;
 }
 else if(strcmp(cmd_list[2],cmd)==0){
 	if(led_status){
-	USART_SendData_IT(&g_usart3, (uint8_t*)"LED:ON\n\r", sizeof("LED:ON\n\r"));
+	USART_SendData_IT(&g_usart3, (uint8_t*)"LED:ON\n\r", sizeof("LED:ON\n\r")-1);
 	}
 	else{
-		USART_SendData_IT(&g_usart3, (uint8_t*)"LED:OFF\n\r", sizeof("LED:OFF\n\r"));
+		USART_SendData_IT(&g_usart3, (uint8_t*)"LED:OFF\n\r", sizeof("LED:OFF\n\r")-1);
 	}
 }
 else{
-	USART_SendData_IT(&g_usart3, (uint8_t*)"cmd not found\n\r", sizeof("cmd not found\n\r"));
+	USART_SendData_IT(&g_usart3, (uint8_t*)"cmd not found\n\r", sizeof("cmd not found\n\r")-1);
 
 }
 
